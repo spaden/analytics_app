5a653852382e82e0389dc9e362ce2ee1ebf288e8
Dheshoju Kalyan Kumar <kalyanspade.n@gmail.com>
2023-02-14 12:13:29 -0500

    API_ready

--- nlp/myapp/app.py
+++ nlp/myapp/app.py
@@ -14,6 +14,7 @@ from sklearn import preprocessing
 
 import pingouin as pg
 
+import json
 
 app = Flask(__name__)
 
@@ -31,7 +32,7 @@ label_encoder = preprocessing.LabelEncoder()
 
 
 def getPredictions(data):
-    
+
     text = remove_noise.remove_noise(data)
 
     smp = tfidf.transform([text]).todense()
@@ -39,58 +40,89 @@ def getPredictions(data):
     return round((gr_model.predict(smp)[0][0] + movie_model.predict(smp)[0][0])/2, 5)
 
 
-def getWordCloud(df, col):
+def getWordCloud(df):
    text = ''
 
-   for wd in df[col]:
+   for wd in df['reviewText']:
       text += ' ' + wd
 
    return text
 
 
-def ratingCount(df, col):
+def ratingCount(df):
 
    res = {}
 
-   for i in range(5):
-      res[i] = df[df[col] == i].count()
+   for i in range(1, 6):
+      res[i] = int(df[df['rating'] == i].count()['rating'])
 
-   return res
+   return json.dumps(res)
 
 
-def rating_over_years(df, rating_col, year_col):
+def rating_over_years(df):
 
    rvRating = df.groupby('year')['rating'].mean().reset_index().sort_values('year', ascending=False)
 
    ctRating = df.groupby('year')['rating'].count().reset_index().sort_values('year', ascending=False)
 
-   return rvRating, ctRating
+   return {
+      'rating': rvRating.to_json(),
+      'ctrating': ctRating.to_json()
+   }
 
 
 
-def rating_diff_over_years(df, rating_col=0, year_col=0):
+def rating_diff_over_years(df):
 
    df_data = df.groupby('year')['rating'].mean().rolling(window=2).apply(lambda x: (x.iloc[1] - x.iloc[0])/x.iloc[0]).fillna(0)
 
-   return df_data
+   return df_data.to_json()
 
 
 def calculate_clt(data):
    tt = []
-   for i in range(100000):
+   for i in range(10):
       tt.append(np.mean(data.sample(50, replace=True)))
    ci = np.percentile(tt, [2.5, 97.5])
-   return ci
+
+   print(list(ci))
+   return ci.tolist()
 
 
 def ptest(df):
-   year_ttest = pg.pairwise_ttests(data=df, dv='rating', between=['year'])
+   lst = df['year'].unique()
 
-   return year_ttest
+   res = {}
 
 
+   for i in range(len(lst)):
+      for j in range(len(lst)):
+
+         if i == j:
+            continue
+
+         try:
+
+            rest = pg.ttest(df[df['year'] == lst[i]]['rating'],
+                            df[df['year'] == lst[j]]['rating'],
+                            correction=True)
+
+            if str(rest['p-val']['T-test']) != 'nan':
+               res[str(lst[i]) + ' - ' + str(lst[j])] = str(rest['p-val']['T-test'])
+
+         except:
+            pass
+   return json.dumps(res)
+
+
+
+@app.route("/getallsentiments", methods=['GET'])
 def get_allsentiments():
+    print('entered')
     df = pd.read_csv('../review_data.csv')
+
+
+    df = df.iloc[:50, :]
 
     df = dateutilmod.generateDates(df)
 
@@ -100,36 +132,51 @@ def get_allsentiments():
 
     df['year'] = df['formatted_date'].dt.year
 
-    df['year'] = pd.to_numeric(df['year'])
+    df['reviewText'].fillna('No Reivew', inplace=True)
 
-    # df['lem_sentiment'] = df['reviewText'].apply(remove_noise)
-    #
-    # df['predsentiment'] = df['lem_sentiment'].apply(getPredictions)
+    df['predsentiment'] = df['reviewText'].apply(getPredictions)
 
-    rating_diff_over_years(df)
-    
-    #print(df['predsentiment'])
-    
+    pTestRes = ptest(df)
+
+
+    cI = calculate_clt(df['rating'])
+
+    ratDiffOverYears = rating_diff_over_years(df)
+
+    ratOverYears = rating_over_years(df)
+
+    ratCount = ratingCount(df)
+
+    worMap = getWordCloud(df)
+
+
+    return {
+       'orgSent': df.to_json(orient ='values'),
+       'wordCloud': worMap,
+       'ratingCount': ratCount,
+       'ratingOverYears': ratOverYears,
+       'ratDifferenceYears': ratDiffOverYears,
+       'confidence_interval': cI,
+        'ptests': pTestRes
+    }
+
 
 
 @app.route("/getsentiment", methods=['POST'])
-def postTest():
+def getSentiment():
    global gr_model, movie_model, tfidf
-    
+
    data = request.json
 
    text = remove_noise.remove_noise(data['userinput'])
 
 
    smp = tfidf.transform([text]).todense()
-   
-   get_allsentiments()
 
    return {
-      'result': 'ok' #[str(gr_model.predict(smp)[0][0]), str(movie_model.predict(smp)[0][0])]
+      'result': [str(gr_model.predict(smp)[0][0]), str(movie_model.predict(smp)[0][0])]
    }
 
 
-
 if __name__ == "__main__":
-    app.run(debug=True, host='127.0.0.1', port = '8002')
+    app.run(debug=True, host='127.0.0.1', port = '8004', threaded=True)
